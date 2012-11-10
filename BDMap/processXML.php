7075eefa604be02d21fa14e6e3a37fa458230b3e
Michelle Torres <licnancytorres@gmail.com>
2012-11-10 14:26:54 -0600

    Genera json de una ruta

--- BDMap/processXML.php
+++ BDMap/processXML.php
@@ -4,9 +4,16 @@ $xml = simplexml_load_file($completeurl);
 
 $placemarks = $xml->Document->Placemark;
  
-for ($i = 0; $i < sizeof($placemarks); $i++) {
-    $coordinates = $placemarks[$i]->LineString->coordinates;
- 
-    echo $coordinates,'<br />';
+$coordinate = $placemarks[0]->LineString->coordinates;
+
+$pares = processXML($coordinate->asXML());
+
+function processXML($cad){
+	$datos["type"] = "LineString"; 
+	$a = split(' ',$cad);
+	foreach($a as $coord)
+		$datos["coordinates"][] = array_map('floatval', explode(',',$coord));
+
+	echo json_encode($datos);
 }
 ?>
