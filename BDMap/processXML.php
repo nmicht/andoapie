c4a7bcb3e0dccbba31685dd8fd7d66664c0ae45c
Michelle Torres <licnancytorres@gmail.com>
2012-11-10 17:28:51 -0600

    Lee todo el kml y genera el json completo

--- BDMap/processXML.php
+++ BDMap/processXML.php
@@ -3,17 +3,77 @@ $completeurl = "kml/ruta.kml";
 $xml = simplexml_load_file($completeurl);
 
 $placemarks = $xml->Document->Placemark;
- 
-$coordinate = $placemarks[0]->LineString->coordinates;
 
-$pares = processXML($coordinate->asXML());
+for ($i = 0; $i < sizeof($placemarks); $i++){
+    $coordinate = $placemarks[$i]->LineString->coordinates;
+	$ruta[] = processXML($coordinate->asXML(),"LineString");
+}
 
-function processXML($cad){
-	$datos["type"] = "LineString"; 
+$placemarks = $xml->Document->Folder->Placemark;
+
+for ($i = 0; $i < sizeof($placemarks); $i++){
+    $coordinate = $placemarks[$i]->Point->coordinates;
+	$terminal[] = processXML($coordinate->asXML(),"Point");
+}
+
+echo $ruta[0],$ruta[1],$terminal[0],$terminal[1];
+
+
+/**
+  * @param string $cad with all the coordinates for a linestring or a point
+  * @return string of a JSON
+  * This function convert the string in arrays and then generate the JSON format
+  */
+function processXML($cad,$object){
+
+	$datos["type"] = $object; 
 	$a = split(' ',$cad);
+
+	//to remove the leading whitespace
+	if($object == "LineString")
+		array_pop($a);
+
 	foreach($a as $coord)
 		$datos["coordinates"][] = array_map('floatval', explode(',',$coord));
 
-	echo json_encode($datos);
+	return json_encode($datos);
 }
+
+/**
+  * This function save on a database the info of the route
+  */
+function saveRoute(){
+	//Connecting to the database	
+	require_once("bd.inc");
+	$conexion = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+	//Finish the execution if the connection fail
+	if ($conexion->connect_error) {
+		die('Error de Conexión (' . $conexion->connect_errno . ') '
+		        . $conexion->connect_error);
+	}
+
+	//Creating the query
+	$query = "INSERT INTO ruta
+				(nombre,
+				derroteroVuelta,
+				derroteroIda,
+				terminal,
+				terminalRetorno)
+			VALUES
+				(
+				<{nombre: }>,
+				<{derroteroVuelta: }>,
+				<{derroteroIda: }>,
+				<{terminal: }>,
+				<{terminalRetorno: }>
+				)
+			";
+
+	//Ejecutar el query
+	$conexion -> query($query);
+
+}
+
+
 ?>
